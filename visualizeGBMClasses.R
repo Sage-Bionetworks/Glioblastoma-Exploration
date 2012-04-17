bdba99b4c5b252932bb04b9817b1490e5a750b54
erichhuang <erich.huang@sagebase.org>
2012-04-17 15:04:08 -0700

    Started on visualizing the molecular classes from a variety of perspectives.

---
+++ visualizeGBMClasses.R
@@ -0,0 +1,16 @@
+# visualizeGBMClasses.R
+
+# Erich S. Huang
+# Sage Bionetworks
+# Seattle, Washington
+# erich.huang@sagebase.org
+
+# NOTES
+# Code to visualize GBM molecular classes as defined by the class discovery
+# object.
+
+### LOAD IN REQUIRED LIBRARIES
+require(HDclassif)
+require(mclust)
+require(ggplot2)
+require(corpcor)
