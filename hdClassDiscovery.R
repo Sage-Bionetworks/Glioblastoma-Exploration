35d48dac4210c91ea11154f4a8f0903f802c4e4f
erichhuang <erich.huang@sagebase.org>
2012-04-17 13:23:00 -0700

    Adding class discovery script.

---
+++ hdClassDiscovery.R
@@ -0,0 +1,10 @@
+# hdClassDiscovery.R
+
+# Erich S. Huang
+# Sage Bionetworks
+# Seattle, Washington
+# erich.huang@sagebase.org
+
+# NOTES
+# Here we take, the data subsetted to transcripts significantly associated with 
+# survival
